022e33ef8738692b9a6d161fe5cff878ea90d35b
Louay Badri <louay.badri@insat.ucar.tn>
2022-07-08 23:58:29 +0100

    skills

--- src/components_v0.2/skills/skills.js
+++ src/components_v0.2/skills/skills.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./skills.css"
 import Skill from "./skills/skill.js"
 
 const Skills = () => {
+    const [index, setIndex] = useState(0)
     return (
         <div className='skills_page page'>
             <h3><Skill /> </h3>
-            <h3><Skill /></h3>
-            <h3><Skill /></h3>
         </div>
     )
 }
--- src/components_v0.2/skills/skills/skill.js
+++ src/components_v0.2/skills/skills/skill.js
@@ -2,11 +2,20 @@ import React from 'react'
 import "./skill.css"
 import { SiFlutter } from "react-icons/si"
 const Skill = () => {
-    return (
-        <div className='skill'>
+    return (<div className='skill_container'>
+        <div className='skill1 skill'>
+            <SiFlutter className='skill_icon simpleShow' />
+            <div>Description </div></div>
+        <div className='skill2 skill'>
             <SiFlutter className='skill_icon simpleShow' />
             <div>Description </div>
         </div>
+        {/* <div className='skill3 skill'>
+            <SiFlutter className='skill_icon simpleShow' />
+            <div>Description </div>
+        </div> */}
+    </div>
+
     )
 }
 
